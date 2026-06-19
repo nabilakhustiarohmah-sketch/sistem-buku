69921d5868e268fae67b91f820b783b39e23d5b5
nabila <nabilakhustiarohmah@saintek.unipdu.ac.id>
2026-06-19 22:53:33 +0700

    Fix: perbaikan sintaks php pembelokan cache tmp

--- api/index.php
+++ api/index.php
@@ -1,9 +1,9 @@
 <?php
 
-// 1. Muat Composer Autoloader
-require __DIR__.'/../vendor/autoload.php';
+// 1. Panggil autoloader bawaan Composer
+require __DIR__ . '/../vendor/autoload.php';
 
-// Paksa Laravel memindahkan jalur penulisan cache ke folder /tmp yang writable di Vercel
+// 2. Paksa Laravel memindahkan jalur penulisan cache ke folder /tmp yang writable di Vercel
 $cachePath = '/tmp/bootstrap/cache';
 if (!file_exists($cachePath)) {
     mkdir($cachePath, 0755, true);
@@ -11,19 +11,9 @@ if (!file_exists($cachePath)) {
 putenv("APP_MANIFEST_CACHE_PATH={$cachePath}/packages.php");
 putenv("APP_SERVICES_CACHE_PATH={$cachePath}/services.php");
 
-// 2. Siapkan folder temporary untuk storage
-if (!isset($_ENV['VAPOR_ARTIFACT_NAME'])) {
-    @mkdir('/tmp/storage/logs', 0755, true);
-    @mkdir('/tmp/storage/framework/views', 0755, true);
-}
+// 3. Jalankan aplikasi Laravel seperti biasa
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// 3. Muat Bootstrap Laravel
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-// 4. Paksa jalur storage ke folder /tmp yang bisa ditulis
-$app->useStoragePath('/tmp/storage');
-
-// 5. Jalankan Aplikasi
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
