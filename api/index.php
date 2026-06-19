884c047d30c956f21c1c917d217f0fb1936967d2
nabila <nabilakhustiarohmah@saintek.unipdu.ac.id>
2026-06-19 08:28:40 +0700

    5

--- api/index.php
+++ api/index.php
@@ -1,3 +1,16 @@
 <?php
 
-require __DIR__.'/../public/index.php';
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+try {
+    $app->handleRequest(Illuminate\Http\Request::capture());
+} catch (\Throwable $e) {
+    echo "<h2>Error:</h2>";
+    echo "<pre>";
+    echo $e->getMessage();
+    echo "\n\nFile: ".$e->getFile();
+    echo "\nLine: ".$e->getLine();
+    echo "</pre>";
+}
