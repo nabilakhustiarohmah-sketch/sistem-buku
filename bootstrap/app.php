5a431995d99c5f0e1abd00d6e1ac286fd8d2e103
nabila <nabilakhustiarohmah@saintek.unipdu.ac.id>
2026-06-19 17:28:43 +0700

    Fix: paksa logging channel menggunakan stderr di Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,11 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+// Paksa log ke stderr jika berjalan di lingkungan serverless Vercel
+if (isset($_SERVER['VERCEL_URL'])) {
+    config(['logging.default' => 'stderr']);
+}
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -15,5 +20,4 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })
-    ->create();
+    })->create();
