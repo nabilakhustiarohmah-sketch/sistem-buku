7fa4d5d164663fb11f6f4a181f0855b88faf48af
nabila <nabilakhustiarohmah@saintek.unipdu.ac.id>
2026-06-19 17:51:17 +0700

    Fix: pindahkan semua storage path ke tmp khusus di vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,12 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-// Paksa log ke stderr jika berjalan di lingkungan serverless Vercel
-if (isset($_SERVER['VERCEL_URL'])) {
-    config(['logging.default' => 'stderr']);
-}
-
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -21,3 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+// TRIK AMPUH: Jika berjalan di Vercel, belokkan folder log ke /tmp
+if (isset($_SERVER['VERCEL_URL'])) {
+    $app->useStoragePath('/tmp');
+}
+
+return $app;
