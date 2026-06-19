9204741f71d0bd4987a11a754bb096e7d3ffeef2
nabila <nabilakhustiarohmah@saintek.unipdu.ac.id>
2026-06-19 15:29:38 +0700

    Bypass public folder path for Vercel serverless

--- api/index.php
+++ api/index.php
@@ -1,6 +1,19 @@
 <?php
 
-chdir(__DIR__ . '/..');
+// Menghubungkan langsung ke core aplikasi tanpa lewat public/index.php luar
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_with_result(__DIR__ . '/../bootstrap/app.php');
 
+function require_with_result($filename) {
+    return require $filename;
+}
 
-require __DIR__ . '/../public/index.php';
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
