cbeb7affa022cc6d5a209ca3ce6d710601b46987
nabila <nabilakhustiarohmah@saintek.unipdu.ac.id>
2026-06-19 08:19:04 +0700

    clear laravel cache

--- api/index.php
+++ api/index.php
@@ -4,14 +4,4 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-try {
-    $app->handleRequest(Illuminate\Http\Request::capture());
-} catch (\Throwable $e) {
-    echo "<pre>";
-    echo $e->getMessage();
-    echo "\n\n";
-    echo $e->getFile();
-    echo " : ";
-    echo $e->getLine();
-    echo "</pre>";
-}
+echo "Laravel Loaded";
