c0b2a43b2d06a3e7eba926b7cdad0b3c96ba6d6b
nabila <nabilakhustiarohmah@saintek.unipdu.ac.id>
2026-06-20 12:06:23 +0700

    okelah

--- api/index.php
+++ api/index.php
@@ -1,17 +1,4 @@
 <?php
 
 // 1. Jalankan core autoload Laravel
-require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-// 3. Tangani request halaman web
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,34 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        // Kosongkan bagian ini
+        //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-/**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        // Pindahkan ke boot() agar konfigurasi config aman diubah saat runtime Vercel
-        if (isset($_SERVER['VERCEL_JOB_ID']) || isset($_SERVER['NOW_REGION'])) {
-            $viewCachePath = '/tmp/storage/framework/views';
-            if (!file_exists($viewCachePath)) {
-                mkdir($viewCachePath, 0755, true);
-            }
-            
-            // GANTI BARIS YANG MERAH TADI DENGAN FUNGSI INI:
-            config(['view.compiled' => $viewCachePath]);
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
         }
     }
 }
